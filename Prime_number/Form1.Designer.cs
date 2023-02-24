097d36e46cdae654746887c8c8f06cd17b4c024c
Maria Alexandra Boros <boros.maria.alexandra@hotmail.com>
2023-02-24 17:28:23 +0200

    Add logic to check if number is prime

--- Prime_number/Form1.Designer.cs
+++ Prime_number/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Prime_number
 {
-    partial class Form1
+    partial class Prime_number
     {
         /// <summary>
         /// Required designer variable.
@@ -29,35 +29,86 @@ namespace Prime_number
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Button = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.numerePrimeCounter = new System.Windows.Forms.Label();
+            this.numereNeprimeCounter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // Button
             // 
-            this.button1.BackColor = System.Drawing.Color.Salmon;
-            this.button1.Location = new System.Drawing.Point(315, 175);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 63);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = false;
+            this.Button.BackColor = System.Drawing.Color.LightSalmon;
+            this.Button.Font = new System.Drawing.Font("Consolas", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button.Location = new System.Drawing.Point(210, 184);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(124, 43);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Submit";
+            this.Button.UseVisualStyleBackColor = false;
+            this.Button.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(210, 158);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(124, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // numerePrimeCounter
+            // 
+            this.numerePrimeCounter.AutoSize = true;
+            this.numerePrimeCounter.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numerePrimeCounter.Location = new System.Drawing.Point(12, 39);
+            this.numerePrimeCounter.Name = "numerePrimeCounter";
+            this.numerePrimeCounter.Size = new System.Drawing.Size(298, 24);
+            this.numerePrimeCounter.TabIndex = 2;
+            this.numerePrimeCounter.Text = "Numere prime inserate: 0";
+            // 
+            // numereNeprimeCounter
+            // 
+            this.numereNeprimeCounter.AutoSize = true;
+            this.numereNeprimeCounter.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numereNeprimeCounter.Location = new System.Drawing.Point(12, 63);
+            this.numereNeprimeCounter.Name = "numereNeprimeCounter";
+            this.numereNeprimeCounter.Size = new System.Drawing.Size(322, 24);
+            this.numereNeprimeCounter.TabIndex = 3;
+            this.numereNeprimeCounter.Text = "Numere neprime inserate: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Prime_number
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(728, 441);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(516, 385);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numereNeprimeCounter);
+            this.Controls.Add(this.numerePrimeCounter);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.Button);
+            this.Name = "Prime_number";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Label numerePrimeCounter;
+        private System.Windows.Forms.Label numereNeprimeCounter;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Prime_number/Form1.cs
+++ Prime_number/Form1.cs
@@ -10,16 +10,49 @@ using System.Windows.Forms;
 
 namespace Prime_number
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+    public partial class Prime_number : Form
         {
-            InitializeComponent();
+
+        int result;
+        int nrPrim = 0;
+        int nrNeprim = 0;
+        public Prime_number() {
+        InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            
 
+            /// MessageBox.Show("S-a introdus " + numberTextBox.Text);
+            bool isNumber = Int32.TryParse(numberTextBox.Text, out result);
+            if (isNumber)
+            { 
+                int i;
+                bool isPrime = true;
+                for (i = 2; i <= result / 2; i++)
+                {
+                    if (result % i == 0)
+                        isPrime = false;
+                }
+                if (isPrime == true)
+                {
+                    this.BackColor = Color.DarkCyan;
+                    nrPrim++;
+                    numerePrimeCounter.Text = "Numere prime introduse: " + nrPrim;
+                }
+                else
+                {
+                    this.BackColor = Color.Salmon;
+                    nrNeprim++;
+                    numereNeprimeCounter.Text = "Numere neprime introduse: " + nrNeprim;
+                }
+
+            }
+            else
+            { 
+                MessageBox.Show("S-a introdus un text: " + numberTextBox.Text);
+                this.BackColor = Color.Cornsilk;
+            }
         }
     }
 }
